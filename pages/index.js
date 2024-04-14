253170c4f02c9263ebb841ee778c5eb65d35f965
Malek <malek.mousa1407@gmail.com>
2024-04-14 14:45:47 +0400

    Initial

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import deved from "../public/dev-ed-wave.png";
-import code from "../public/code.png";
-import design from "../public/design.png";
-import consulting from "../public/consulting.png";
+import deved from "../public/images.jpeg";
+import code from "../public/human-trafficking.png";
+import design from "../public/impact.png";
+import consulting from "../public/victim.png";
 import Image from "next/image";
 import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
@@ -19,7 +19,7 @@ import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -31,7 +31,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <h1 className="font-burtons text-xl">developed by malek</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -40,38 +40,35 @@ export default function Home() {
                 />
               </li>
               <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
+               
               </li>
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+              Human Trafficking
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+              What is human trafficking?
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+              Human trafficking is a criminal activity where individuals 
+              are coerced, deceived, or forced into situations against 
+              their will for purposes such as forced labor, sexual exploitation, 
+              or other forms of exploitation.
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+            {/* <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <AiFillTwitterCircle />
               <AiFillLinkedin />
               <AiFillYoutube />
-            </div>
+            </div> */}
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={deved} layout="fill" objectFit="cover" />
             </div>
           </div>
         </section>
         <section>
-          <div>
+          {/* <div>
             <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
@@ -85,75 +82,60 @@ export default function Home() {
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
-          </div>
+          </div> */}
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
               <Image src={design} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
+                Causes of Human Trafficking
               </h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+
+               <h4 className="py-4 text-teal-600"> A complex set of factors fuel human trafficking.</h4>
+                <p className="text-gray-800 py-1">Marginalized groups, including women, children, migrants, and minorities, are often targeted due to their disadvantaged social status.</p>
+                <br/>
+                <p className="text-gray-800 py-1">Conflict zones and areas with weak governance create environments conducive to trafficking, as law enforcement and protection mechanisms may be absent or compromised.</p>
+                <br/>
+                <p className="text-gray-800 py-1">Poverty and lack of economic opportunities can push individuals into situations where they are susceptible to trafficking.</p>
+                <br/>
+ 
             </div>
+
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={code} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
+                Types of Trafficking
               </h3>
-              <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+
+              <h4 className="py-4 text-teal-600">There exists multiple type of trafficking, including:</h4>
+              <p className="text-gray-800 py-1"><b>Labor Trafficking:</b> Individuals are coerced or deceived into working under exploitative conditions, often without proper compensation, freedom, or legal protections.</p>
+              <br/>
+              <p className="text-gray-800 py-1"><b>Organ Trafficking:</b> Trafficking for the purpose of illegal organ removal and transplantation involves the exploitation of individuals for their organs.</p>
+              <br/>
+              <p className="text-gray-800 py-1"><b>Trafficking for Begging:</b> Traffickers may coerce or deceive individuals into begging on the streets, exploiting sympathy from the public while pocketing the proceeds for themselves.</p>
             </div>
+
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Impact on Victims</h3>
+
+              <h4 className="py-4 text-teal-600">Perpetrators employ tactics that can traumatize their victims:</h4>
+              <p className="text-gray-800 py-1">Perpetrators expose the victim to high amounts of psychological stress induced by threats, fear, and physical and emotional violence.</p>
+              <br/>
+              <p className="text-gray-800 py-1">Psychological reviews have shown that the chronic stress experienced by many victims of human trafficking can compromise the immune system.</p>
+              <br/>
+              <p className="text-gray-800 py-1">Victims experience severe trauma on a daily basis that devastates the healthy development of self-concept, self-worth, biological integrity, and cognitive functioning.</p>
             </div>
           </div>
         </section>
         <section className="py-10">
-          <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
+
+        <h3 className="text-3xl py-1 dark:text-white ">Relevant Statistics</h3>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">            
+            <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
-                height={"100%"}
+                height={"110%"}
                 layout="responsive"
                 src={web1}
               />
@@ -161,49 +143,40 @@ export default function Home() {
             <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-lg object-cover"
-                width={"100%"}
+                width={"200%"}
                 height={"100%"}
                 layout="responsive"
                 src={web2}
               />
             </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web3}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web4}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-              />
-            </div>
           </div>
+
+          <div>
+            <h4 className="text-3xl py-1 dark:text-white ">Initiatives Undertaken by Organizations</h4>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            <span className="text-teal-500">The 24 Hour Race </span> is one such initiative that focuses on increasing awareness among high 
+            school students in Asia. </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            <span className="text-teal-500">The Blue Campaign</span> is another anti-trafficking initiative that works with the U.S. 
+            Department of Homeland Security to combat human trafficking and bring freedom to exploited victims.
+            </p>
+
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            <span className="text-teal-500">Dubai Foundation for Women and Children</span> received the Chaillot Prize in 2019 for the 
+              “Awareness Program for the Most Vulnerable Groups to Human Trafficking” and for enhancing 
+              human rights in the GCC.
+            </p>
+          </div>
+            <br/>
+          <div>
+            <h4 className="text-3xl py-1 dark:text-white ">Conclusion</h4>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">Each type of trafficking involves the exploitation and abuse of individuals for profit, 
+              with traffickers using various tactics to control and manipulate their victims. 
+              Efforts to combat trafficking require tailored strategies to address the specific dynamics of each type, 
+              including prevention, prosecution of perpetrators, protection of victims, and support for survivors.</p>
+          </div>
+
+          
         </section>
       </main>
     </div>
